83103c79361749342ab49b6d21d6fd68018ea25b
Diego Lopez <dfelo93@gmail.com>
2020-12-07 15:42:22 -0500

    Removed useless for and tightened lines

--- 0x00-python-hello_world/10-check_cycle.c
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -16,17 +16,14 @@
 int check_cycle(listint_t *list)
 {
 	listint_t *turtle = list, *hare = list;
-	int i;
 
 	if(!list || !list->next)
 		return (0);
 	while(hare && turtle)
 	{
-		turtle = turtle->next;
-		hare = hare->next->next;
+		turtle = turtle->next, hare = hare->next->next;
 		if(turtle == hare)
 			return (1);
 	}
-	for (i = 0; i < 10; i++);
 	return (0);
 }
