cceb696961281734c3ad5d4912c43905868c93da
Diego Lopez <dfelo93@gmail.com>
2020-12-07 15:16:07 -0500

    Checks if there is a cycle on a linked list

---
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -0,0 +1,30 @@
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include "lists.h"
+/**
+ * check_cycle - Checks if a singly linked list has a cycle.
+ *
+ *  Arguments:
+ *    @list:   - Head pointer to the linked list.
+ *
+ *   Return:   - 0 If no cycle exists or 1 if one is found.
+ *
+ * |--------------- Written by Diego Lopez ---------------|
+ * |----------------- December - 7 - 2020 ----------------|
+ */
+int check_cycle(listint_t *list)
+{
+	listint_t *turtle = list, *hare = list;
+	if(list && !list->next)
+	{
+		while(hare && turtle)
+		{
+			turtle = turtle->next;
+			hare = hare->next->next;
+			if(turtle == hare)
+				return (1);
+		}
+	}
+	return (0);
+}
