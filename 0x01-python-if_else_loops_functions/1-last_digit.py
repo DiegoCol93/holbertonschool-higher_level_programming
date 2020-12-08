4aa12912555dde7a1c49174734319f01da74fb82
Diego Lopez <dfelo93@gmail.com>
2020-12-08 12:06:53 -0500

    Fixed PEP8 style

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -2,11 +2,11 @@
 import random
 number = random.randint(-10000, 10000)
 str_num = str(number)
-print("Last digit of {0:d} is".format(number), end =' ')
+print("Last digit of {0:d} is".format(number), end=' ')
 if number > 0:
-    print("{0:s}".format(str_num[-1:]), end =' ')
+    print("{0:s}".format(str_num[-1:]), end=' ')
 if number < 0 and str_num[-1:] != '0':
-    print("-{0:s}".format(str_num[-1:]), end =' ')
+    print("-{0:s}".format(str_num[-1:]), end=' ')
 last_digit = int(str(number)[-1])
 if last_digit > 5:
     print("and is greater than 5")
