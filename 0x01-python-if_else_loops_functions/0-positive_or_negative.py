c2d993ffc75bc54f610e11709cfbf8fc074253e9
Diego Lopez <dfelo93@gmail.com>
2020-12-08 12:04:11 -0500

    Fixed PEP8 style

--- 0x01-python-if_else_loops_functions/0-positive_or_negative.py
+++ 0x01-python-if_else_loops_functions/0-positive_or_negative.py
@@ -2,8 +2,8 @@
 import random
 number = random.randint(-10, 10)
 if number > 0:
-    print(number, end =" is positive\n")
+    print(number, end=" is positive\n")
 if number == 0:
-    print(number, end =" is zero\n")
+    print(number, end=" is zero\n")
 if number < 0:
-    print(number, end =" is negative\n")
+    print(number, end=" is negative\n")
