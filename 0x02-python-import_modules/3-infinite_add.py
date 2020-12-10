e7fe36379dd0e11f940fb1fd7d244b1cf1d68ff7
Diego Lopez <dfelo93@gmail.com>
2020-12-10 13:14:21 -0500

    Adds two numbers as arguments.

---
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    from sys import argv
+
+# Get number of arguments.
+    n_args = len(argv)
+
+# if no arguments.
+    if n_args == 1:
+        print("0")
+
+# if one argument.
+    elif n_args == 2:
+        print("{}".format(argv[1]))
+
+# if more arguments.
+    elif n_args > 2:
+        result = 0
+        for argument in argv[1:]:
+            result = result + int(argument)
+    print(result)
