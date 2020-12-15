9f67807ccbe9d83ad34383eed0e9e9928c9df062
Diego Lopez <dfelo93@gmail.com>
2020-12-15 12:24:22 -0500

    Adds 2 tuples.

---
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+def add_tuple(tuple_a=(), tuple_b=()):
+    res_tuple = (0, 0)
+    tuple_a += (0, 0)
+    tuple_b += (0, 0)
+    res_tuple = ((tuple_a[0] + tuple_b[0]),
+                 (tuple_a[1] + tuple_b[1]))
+    return res_tuple
