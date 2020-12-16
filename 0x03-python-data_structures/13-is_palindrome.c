4dbe798b3c970796c209cf5cce4dea0fecf1bceb
Diego Lopez <dfelo93@gmail.com>
2020-12-15 20:03:56 -0500

    Changed position of NULL condition and return

--- 0x03-python-data_structures/13-is_palindrome.c
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -10,31 +10,29 @@ int is_palindrome(listint_t **head)
 	listint_t *ptr1 = NULL;
 	listint_t *ptr2 = NULL;
 
-	if (head && *head)
+	if (!head || !*head)
+		return (1);
+	ptr1 = *head;
+	ptr2 = *head;
+	len = get_lenght(head) - 1;
+	if (len % 2 == 0)
+		half = (len / 2);
+	else
+		half = (len / 2) + 1;
+	for (; half > 0; half--)
 	{
-		ptr1 = *head;
 		ptr2 = *head;
-		len = get_lenght(head) - 1;
-		if (len % 2 == 0)
-			half = (len / 2);
-		else
-			half = (len / 2) + 1;
-		for (; half > 0; half--)
-		{
-			ptr2 = *head;
-			cmp = len;
-			for (; cmp > 0; cmp--)
-				ptr2 = ptr2->next;
-			if (ptr1->n != ptr2->n)
-				break;
-			ptr1 = ptr1->next;
-			len --;
-		}
-		if (half == 0 && cmp == 0)
-			return (1);
-		return (0);
+		cmp = len;
+		for (; cmp > 0; cmp--)
+			ptr2 = ptr2->next;
+		if (ptr1->n != ptr2->n)
+			break;
+		ptr1 = ptr1->next;
+		len --;
 	}
-	return (1);
+	if (half == 0 && cmp == 0)
+		return (1);
+	return (0);
 }
 unsigned int get_lenght(listint_t **head)
 {
