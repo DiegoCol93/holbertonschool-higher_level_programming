f0848f2e3fb8deea113f604bebaff29c4cef1cd9
Diego Lopez <dfelo93@gmail.com>
2020-12-15 23:49:21 -0500

    Addewd recursion to prevent loop

--- 0x03-python-data_structures/13-is_palindrome.c
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -1,29 +1,18 @@
 #include "lists.h"
 #include <stdio.h>
+#include <stdlib.h>
+
+int get_lenght_recursive(listint_t *copy, int n);
 int is_palindrome(listint_t **head)
 {
+	listint_t *ptr1 = NULL, *ptr2 = NULL;
 	unsigned int len = 0, half = 0, cmp = 0;
-	listint_t *ptr1 = NULL;
-	listint_t *ptr2 = NULL;
 
 	if (!head || !*head)
 		return (1);
 	ptr1 = *head;
 	ptr2 = *head;
-	while (ptr2)
-	{
-		if (ptr2->next)
-		{
-			ptr2 = ptr2->next->next;
-			len += 2;
-		}
-		else
-		{
-			ptr2 = ptr2->next;
-			len++;
-		}
-	}
-	len--;
+	len = get_lenght_recursive(ptr2, 0);
 	if (len % 2 == 0)
 		half = (len / 2);
 	else
@@ -32,8 +21,8 @@ int is_palindrome(listint_t **head)
 	{
 		ptr2 = ptr1;
 		cmp = len;
-		/*	for (; cmp > 0; cmp--)
-			ptr2 = ptr2->next;*/
+		for (; cmp > 0; cmp--)
+			ptr2 = ptr2->next;
 		if (ptr1->n != ptr2->n)
 			break;
 		ptr1 = ptr1->next;
@@ -42,4 +31,13 @@ int is_palindrome(listint_t **head)
 	if (half == 0 && cmp == 0)
 		return (1);
 	return (0);
+}
+int get_lenght_recursive(listint_t *copy, int n)
+{
+        if(!copy->next)
+		return (n);
+	else
+		return (n);
+        copy = copy->next;
+        get_lenght_recursive(copy, n);
 }
