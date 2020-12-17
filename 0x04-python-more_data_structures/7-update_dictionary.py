7283a760c878e1c1ddbd9d9a095122cb157b39d8
Diego Lopez <dfelo93@gmail.com>
2020-12-17 13:39:54 -0500

    Updates original dictionary, not a copy of it

--- 0x04-python-more_data_structures/7-update_dictionary.py
+++ 0x04-python-more_data_structures/7-update_dictionary.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
 def update_dictionary(a_dictionary, key, value):
-    a_new_dictionary = a_dictionary.copy()
-    a_new_dictionary[key] = value
-    return(a_new_dictionary)
+    a_dictionary["{}".format(key)] = value
+    return(a_dictionary)
