88142a6159cf79d6a1a0ecd5c7bad2384024bbd6
Diego Lopez <dfelo93@gmail.com>
2020-12-17 14:58:35 -0500

    Gets the best score of a student on a dictionary.

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
 def best_score(a_dictionary):
     if a_dictionary is not None:
-        x = (max(sorted(a_dictionary)))
-        return x
+        return max(sorted(a_dictionary)))
     else:
         return None
