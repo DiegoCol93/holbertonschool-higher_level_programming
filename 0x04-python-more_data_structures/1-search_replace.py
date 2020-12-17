b9c2196aff0be0ef670cd23e258f0d49f50afbd1
Diego Lopez <dfelo93@gmail.com>
2020-12-17 11:44:25 -0500

    Searches and replaces a value on a list

---
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+def search_replace(my_list, search, replace):
+
+    new_list = my_list.copy()
+    i = 0
+
+    while i < len(my_list):
+        if my_list[i] == search:
+            new_list[i] = replace
+
+        i += 1
+    return new_list
