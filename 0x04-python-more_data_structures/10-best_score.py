84edbb9edd081af9a2b8028b57920ebda8e366b9
Diego Lopez <dfelo93@gmail.com>
2020-12-17 15:20:22 -0500

    Remove is not None

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def best_score(a_dictionary):
-    if a_dictionary is not None and a_dictionary.get:
+    if a_dictionary and a_dictionary.get:
         top = sorted(a_dictionary, key=a_dictionary.get, reverse=True)
         return (top[0])
     else:
