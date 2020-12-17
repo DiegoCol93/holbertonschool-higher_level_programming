7b260b35d4f3b88ceb7dbf9e3521d9a6607c68ff
Diego Lopez <dfelo93@gmail.com>
2020-12-17 15:08:11 -0500

    Sort by value and proper return.

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 def best_score(a_dictionary):
     if a_dictionary is not None:
-        return max(sorted(a_dictionary))
+        top = sorted(a_dictionary, key=a_dictionary.get, reverse=True)
+        return (top[0])
     else:
         return None
