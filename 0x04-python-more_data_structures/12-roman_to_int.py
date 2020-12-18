a6b8c5ae3c8efd1e114c7b1b4bdc0a3b079a4345
Diego Lopez <dfelo93@gmail.com>
2020-12-17 19:20:46 -0500

    Better return

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -4,7 +4,7 @@ def roman_to_int(roman_string):
     let_dic = {'M': 1000, 'D': 500, 'C': 100, 'L': 50, 'X': 10, 'V': 5, 'I': 1}
     res = 0
     i = 0
-    if isinstance(roman_string,str)and roman_string is not None:
+    if isinstance(roman_string, str) and roman_string is not None:
         while i < len(roman_string):
             if (i < (len(roman_string) - 1) and
                 let_dic[roman_string[i + 1]] > let_dic[roman_string[i]]):
@@ -12,6 +12,4 @@ def roman_to_int(roman_string):
             else:
                 res += let_dic[roman_string[i]]
             i += 1
-        return res
-    return None
-
+    return res
