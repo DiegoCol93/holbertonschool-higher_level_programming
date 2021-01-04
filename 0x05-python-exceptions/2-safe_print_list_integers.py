2aefc7ffd12cce9a82c589a39c261361070a8958
Diego Lopez <dfelo93@gmail.com>
2021-01-04 13:12:12 -0500

    Redo of logic and added TypeError

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python3
 def safe_print_list_integers(my_list=[], x=0):
     ints = 0
-    while ints < x:
+    for value in range(0, x):
         try:
-            print("{:d}".format(my_list[ints]), end='')
-        except ValueError:
+            print("{:d}".format(my_list[value]), end='')
+            ints += 1
+        except (ValueError, TypeError):
             pass
-        ints += 1
     print()
     return ints
