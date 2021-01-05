58a4662739bba2c47c389a14d2261d4859013ee1
Diego Lopez <dfelo93@gmail.com>
2021-01-05 17:35:09 -0500

    Removed float condition.

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -7,7 +7,7 @@ class Square:
     def __init__(self, size=0):
         ''''Args:
     size (int): Size of the square.'''
-        if isinstance(size, (int, float)) is True:      # Is size a digit ?
+        if isinstance(size, (int)) is True:      # Is size a digit ?
             if size < 0:                                # Is size less than 0 ?
                 raise ValueError("size must be >= 0")
             else:
