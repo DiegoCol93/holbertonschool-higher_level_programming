8805710f17207ded0c28992037b4131e43290274
Diego Lopez <dfelo93@gmail.com>
2021-01-05 17:22:56 -0500

    Added proper commenting.

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+''' Module: For defining the Square class. '''
+
+
+class Square:
+    ''' Class: For defining a Square object. '''
+    def __init__(self, size):
+        '''Args:
+     size (int): Size of the square.'''
+        self.__size = size
