72803a95f5b94f91d6f4a47076c9fec80b299a66
Diego Lopez <dfelo93@gmail.com>
2021-01-05 17:37:17 -0500

    Proper commenting.

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -7,10 +7,10 @@ class Square:
     def __init__(self, size=0):
         ''''Args:
     size (int): Size of the square.'''
-        if isinstance(size, (int)) is True:      # Is size a digit ?
-            if size < 0:                                # Is size less than 0 ?
+        if isinstance(size, (int)) is True:
+            if size < 0:
                 raise ValueError("size must be >= 0")
             else:
-                self.__size = size                      #: init attribute.
+                self.__size = size  #: init attribute.
         else:
             raise TypeError("size must be an integer")
