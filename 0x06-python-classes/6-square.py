19e12447fa4acd9da5445650fdb0739e4e2a9a82
Diego Lopez <dfelo93@gmail.com>
2021-01-05 22:36:31 -0500

    Removed stray or.

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -72,7 +72,7 @@ class Square:
                 type(value[0]) is not int or
                 type(value[1]) is not int or
                 value[0] < 0 or
-                value[1] < 0 or):
+                value[1] < 0):
             raise TypeError("position must be a tuple of 2 positive integers")
         else:
             self.__position = value
