36b6f515b1d0d499688b5f35803aed318c1daa9a
Diego Lopez <dfelo93@gmail.com>
2021-01-12 18:25:22 -0500

    Added documentation.

--- 0x09-python-everything_is_object/101-locked_class.py
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -1,4 +1,8 @@
 #!/usr/bin/python3
+""" Module for storing a LockedClass. """
+
+
 class LockedClass():
+    """ LockedClass, can only have first_name as attribute. """
     __slots__ = ['first_name']
     pass
