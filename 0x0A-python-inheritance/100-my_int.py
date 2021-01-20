3570a2dfbffd78ce59d5d73bd0230671249a125c
Diego Lopez <dfelo93@gmail.com>
2021-01-19 22:58:43 -0500

    Casting into integers.

---
+++ 0x0A-python-inheritance/100-my_int.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+""" Module for storing MyInt clss. """
+
+
+class MyInt(int):
+    """ My integer inherits from integer """
+
+    def __eq__(self, other):
+        """ equal to """
+        return False if (int(self) is other) else True
+
+    def __ne__(self, other):
+        """  """
+        return True if (int(self) is other) else False
