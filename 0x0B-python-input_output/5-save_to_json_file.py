86743a6bde447db00120c9d3ac8d9b84949f0ec7
Diego Lopez <dfelo93@gmail.com>
2021-01-20 19:04:20 -0500

    Added proper comments.

--- 0x0B-python-input_output/5-save_to_json_file.py
+++ 0x0B-python-input_output/5-save_to_json_file.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
-"""  """
+""" Module for storing the save_to_jason_file """
 import json
 
 
 def save_to_json_file(my_obj, filename):
-    """  """
+    """ Saves an Object as JSON string format into a text file.  """
     with open(filename, "w") as json_txt_file:
         json_txt_file.write(json.dumps(my_obj))
