567e7f39444da65f94e6f9f3241b3c39f2e7ab82
Diego Lopez <dfelo93@gmail.com>
2021-01-20 21:19:46 -0500

    Module including Student class.

---
+++ 0x0B-python-input_output/9-student.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""  """
+
+
+class Student():
+    """  """
+    def __init__(self, first_name, last_name, age):
+        """ __init__ :
+
+            Args:
+                first_name(str): Student's first_name
+                last_name(str): Student's last_name
+                age(str): Student's age
+
+        """
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self):
+        """ Returns the dictionary representation of a Student instance """
+        return self.__dict__
