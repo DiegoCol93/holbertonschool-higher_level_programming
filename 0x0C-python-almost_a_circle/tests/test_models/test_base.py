b985480452daf7b3ac9e0c063b33904715e724aa
Diego Lopez <dfelo93@gmail.com>
2021-01-26 09:50:37 -0500

    Updated test cases.

---
+++ 0x0C-python-almost_a_circle/tests/test_models/test_base.py
@@ -0,0 +1,73 @@
+#!/usr/bin/python3
+""" Test module for storing Base class test cases. """
+import unittest
+from models.base import Base
+from models.rectangle import Rectangle
+from unittest.mock import patch
+from io import StringIO
+from time import sleep
+
+
+print_on = 0
+class TestBase(unittest.TestCase):
+    """ TestBase class for storing the unittest methods and cases. """
+    if print_on == 1:
+        green = "\033[92m"  # <-- Stores the green text color format.
+        reset = "\033[0m"  # <-- Stores the reset text color format.
+        print(green + "." + "~ " * 11 + "~| test_base.py module. | " +
+              "~ " * 11 + reset)
+        sleep(1)
+
+    # Tests from 0-main.py----------------------------------------------------|
+    def test_0_Base(self):
+        """ Tests the cases for the Base class from 0-main.py """
+        if print_on == 1:
+            green = "\033[92m"
+            reset = "\033[0m"
+            print(green + '.' + "~" * 20 + " Testing cases from 0-main.py " +
+                  "~" * 19 + reset)
+        b1 = Base()
+        self.assertEqual(b1.id, 1)
+        b2 = Base()
+        self.assertEqual(b2.id, 2)
+        b3 = Base()
+        self.assertEqual(b3.id, 3)
+        b4 = Base(12)
+        self.assertEqual(b4.id, 12)
+        b5 = Base()
+        self.assertEqual(b5.id, 4)
+
+    # Tests from 14-main.py----------------------------------------------------|
+    def test_14_Base(self):
+        """ Tests the cases for the Base class from 14-main.py """
+        if print_on == 1:
+            green = "\033[92m"
+            reset = "\033[0m"
+            print(green + '.' + "~" * 20 + " Testing cases from 14-main.py " +
+                  "~" * 19 + reset)
+
+        r1 = Rectangle(10, 7, 2, 8)
+        dictionary = r1.to_dictionary()
+        json_dictionary = Base.to_json_string([dictionary])
+
+        self.assertEqual(json_dictionary,
+        '[{"x": 2, "width": 10, "id": 1, "height": 7, "y": 8}]')
+        print(dictionary)
+        print(type(dictionary))
+        print(json_dictionary)
+        print(type(json_dictionary))
+
+    # Tests for the Base class instance. -------------------------------------|
+    def test_instance(self):
+        """ Test cases for the Base class instance and it's types """
+        b1 = Base()
+        self.assertIsInstance(b1, Base)
+        self.assertTrue(type(b1) == Base)
+
+    # Teardown method for resetting the count of instances in Base class.-----|
+    def tearDown(self):
+        """ Resets the Base class counter after each test unit. """
+        Base._Base__nb_objects = 0
+
+if __name__ == '__main__':
+    unittest.main()
