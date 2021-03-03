b3bd8d2b9e5e9d10e7024a3293aea4b88b8aa13a
DiegoCol93 <dfelo93@gmail.com>
2021-03-02 22:40:48 -0500

    ADDED | Lists all shows contained in the database without a genre.

---
+++ 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
@@ -0,0 +1,7 @@
+-- Lists all shows contained in the database without a genre.
+SELECT tv_genres.name AS genres,
+       COUNT(tv_show_genres.genre_id) AS number_of_shows 
+FROM tv_show_genres, tv_genres
+WHERE tv_genres.id = tv_show_genres.genre_id
+GROUP BY tv_show_genres.genre_id, tv_genres.name
+ORDER BY number_of_shows DESC
