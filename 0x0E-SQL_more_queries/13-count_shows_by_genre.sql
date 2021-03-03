35ee2b61e4afa7a42e97cac59babaa392e0cc622
DiegoCol93 <dfelo93@gmail.com>
2021-03-02 22:42:17 -0500

    FIX | Name of field from genres to genre - line 2.

--- 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
+++ 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
@@ -1,5 +1,5 @@
 -- Lists all shows contained in the database without a genre.
-SELECT tv_genres.name AS genres,
+SELECT tv_genres.name AS genre,
        COUNT(tv_show_genres.genre_id) AS number_of_shows 
 FROM tv_show_genres, tv_genres
 WHERE tv_genres.id = tv_show_genres.genre_id
