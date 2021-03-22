34fdb1c3432f0e84b20b4e9fc0ec8762f69b7f92
DiegoCol93 <dfelo93@gmail.com>
2021-03-22 12:23:31 -0500

    ADDED | Prints 3 lines.

---
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -0,0 +1,9 @@
+#!/usr/bin/node
+
+// Declare array of phrases.
+const myArray = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
+
+// Iterate through array.
+myArray.forEach(function (phrase, index, myArray) {
+  console.log(phrase);
+});
