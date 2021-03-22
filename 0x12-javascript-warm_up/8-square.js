128fe065f5072a6a08a152bbf7a1a09d9e46a8df
DiegoCol93 <dfelo93@gmail.com>
2021-03-22 18:33:38 -0500

    FIXED | Removed extra for and used .repeat() string method.

--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -7,11 +7,8 @@
 if (isNaN(process.argv[2])) {
   console.log('Missing size');
 } else {
-  // For i and j = number given and are more than 0.
+  // For i = given number and greater than 0.
   for (let i = Number(process.argv[2]); i > 0; i--) {
-    for (let j = Number(process.argv[2]); j > 0; j--) {
-      process.stdout.write('x');
-    }
-    console.log();
+    console.log('x'.repeat(process.argv[2]));
   }
 }
