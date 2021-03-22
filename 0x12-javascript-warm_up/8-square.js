08d94596a2612c0a7c2e0e0015b01a5225302c17
DiegoCol93 <dfelo93@gmail.com>
2021-03-22 18:50:07 -0500

    FIXED | Casted properly the given argv variable.

--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -4,11 +4,11 @@
 //   Useage: ./8-square.js {# Size of square}
 
 // If Not A Number.
-if (isNaN(process.argv[2])) {
+if (isNaN(parseInt(process.argv[2]))) {
   console.log('Missing size');
 } else {
   // For i = given number and greater than 0.
-  for (let i = Number(process.argv[2]); i > 0; i--) {
-    console.log('x'.repeat(process.argv[2]));
+  for (let i = parseInt(process.argv[2]); i > 0; i--) {
+    console.log('x'.repeat(parseInt(process.argv[2])));
   }
 }
