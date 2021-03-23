5230b1aa0775a3f835beda8187d85908f8e712f1
DiegoCol93 <dfelo93@gmail.com>
2021-03-22 20:59:24 -0500

    FIXED | Moved function definition out of conditional statement.

--- 0x12-javascript-warm_up/10-factorial.js
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -1,21 +1,18 @@
 #!/usr/bin/node
 // Computes and prints the factorial of a given number.
 
+// Define recursive factorial function.
+function factorial (n) {
+  let result = 1;
+  if (n > 0) {
+    result = n * factorial(n - 1); // Begin recursion, result = n * (n - 1) * (n - 2)...
+  }
+  return result;
+}
 // Check if Not A Number.
 if (isNaN(parseInt(process.argv[2]))) {
   console.log(1);
 } else {
-  // Define recursive factorial function.
-  function factorial (n) {
-    let result = 1;
-    // Base Case.
-    if (n > 0) {
-      // Begin recursion.
-      // result = n * (n - 1) * (n - 2)...
-      result = n * factorial(n - 1);
-    }
-    return result;
-  }
   // Call factorial function.
   console.log(factorial(parseInt(process.argv[2])));
 }
