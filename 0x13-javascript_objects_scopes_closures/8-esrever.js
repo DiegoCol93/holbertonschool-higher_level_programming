eb85671394024887bc2e24cda9ec7d55f91f0228
DiegoCol93 <dfelo93@gmail.com>
2021-03-23 14:20:52 -0500

    ADDED | Returns the reversed version of a list, without using .reverse()

--- 0x13-javascript_objects_scopes_closures/8-esrever.js
+++ 0x13-javascript_objects_scopes_closures/8-esrever.js
@@ -1,4 +1,9 @@
 #!/usr/bin/node
 exports.esrever = function (list) {
-  return (list.reverse());
+  const rev = [];
+  let j = 0;
+  for (let i = list.length - 1; i >= 0; i--, j++) {
+    rev[j] = list[i];
+  }
+  return rev;
 };
