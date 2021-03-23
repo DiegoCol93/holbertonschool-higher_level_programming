2e5fb6e6a4a286efb71ce95fc8536d24b6ec23c3
DiegoCol93 <dfelo93@gmail.com>
2021-03-23 12:20:32 -0500

    ADDED | print() method.

---
+++ 0x13-javascript_objects_scopes_closures/3-rectangle.js
@@ -0,0 +1,14 @@
+#!/usr/bin/node
+module.exports = class Rectangle {
+  constructor (w, h) {
+    if (w > 0 && h > 0) {
+      this.width = w;
+      this.height = h;
+    }
+  }
+  print () {
+    for (let i = this.height; i > 0; i--) {
+      console.log('x'.repeat(this.width));
+    }
+  }
+};
