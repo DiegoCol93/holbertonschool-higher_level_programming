277c7dfebf2cfa9b25b56214f85cd19b4afa23bb
DiegoCol93 <dfelo93@gmail.com>
2021-03-27 00:30:19 -0500

    ADDED | Displays all values in the states table of hbtn_0e_0_usa where name matches the argument.

---
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+''' Module for storing the 2. Filter states by user input task script. '''
+
+if __name__ == "__main__":
+
+    import MySQLdb
+    from sys import argv as av
+
+    args = {
+        "host": "localhost",  # <-- Host Name.
+        "port": 3306,  # <- - - --- Port Number.
+        "user": av[1],  # <- - - -- User name.
+        "passwd": av[2],  # <- - -- User password.
+        "db": av[3],  # <- - - - -- Database name.
+        "charset": "utf8"  # <- --- Character set.
+    }
+
+    # Create connection with database.
+    with MySQLdb.connect(**args) as cursor:
+
+        # SELECT * WHERE name matches the given argument.
+        cursor.execute("SELECT * FROM states WHERE name='{}'".format(av[4]))
+
+        # Fetch all content and return it as tuple object.
+        rows = cursor.fetchall()
+        for row in rows:
+            print(row)
