78f5e01a0917350c41f728e391e8d4caa5f8e643
DiegoCol93 <dfelo93@gmail.com>
2021-03-27 02:23:33 -0500

    ADDED | Lists all City objects from the database hbtn_0e_101_usa.

---
+++ 0x0F-python-object_relational_mapping/102-relationship_cities_states_list.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+''' Start link class to table in database '''
+
+if __name__ == "__main__":
+    from relationship_state import Base, State
+    from relationship_city import City
+    from sys import argv as av
+    from sqlalchemy import (create_engine)
+    from sqlalchemy.orm import Session
+
+    # Create engine connection. av[1]=Username av[2]=Password av[3]=DBName.
+    engine = create_engine('mysql+mysqldb://' + '{}'.format(av[1]) +
+                           ':{}'.format(av[2]) + '@localhost/' +
+                           '{}'.format(av[3]), pool_pre_ping=True)
+
+    # Creates all classes currently active.
+    Base.metadata.create_all(engine)
+
+    # Create session cursor.
+    session = Session(engine)
+
+    # Create Query.
+    args = [State.id, State.name, City.id, City.name]
+    queryTable = session.query(*args).join(City)
+
+    # Print on a specific format.
+    for row in queryTable:
+        print("{}: {} -> {}".format(row[2], row[3], row[1]))
+
+    # Close session cursor.
+    session.close()
