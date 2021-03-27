54746aa80112e284ea6b75e3addbaa623c496728
DiegoCol93 <dfelo93@gmail.com>
2021-03-27 01:06:41 -0500

    ADDED | Prints the State object with the name passed as argument from the database hbtn_0e_6_usa.

---
+++ 0x0F-python-object_relational_mapping/10-model_state_my_get.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+''' Start link class to table in database '''
+
+if __name__ == "__main__":
+    from model_state import Base, State
+    from sys import argv as av
+    from sqlalchemy import (create_engine)
+    from sqlalchemy.orm import Session
+
+    # Create engine connection. av[1]=Username av[2]=Password av[3]=DBName.
+    engine = create_engine('mysql+mysqldb://' + '{}'.format(av[1]) +
+                           ':{}'.format(av[2]) + '@localhost/' +
+                           '{}'.format(av[3]), pool_pre_ping=True)
+
+    # Creates all classes currently active.
+    Base.metadata.create_all(engine)
+
+    # Create session cursor.
+    session = Session(engine)
+
+    st = session.query(State).order_by(State.id).filter(State.name == av[4])
+    for state in st:
+        print(state.id)
+
+    # Close session cursor.
+    session.close()
