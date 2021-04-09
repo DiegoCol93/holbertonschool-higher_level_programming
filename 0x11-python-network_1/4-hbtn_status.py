468863a62f0bea1210a7d5c2bc8b2e245c8d2479
DiegoCol93 <dfelo93@gmail.com>
2021-04-09 18:33:11 -0500

    | ADDED | Gets the content of https://intranet.hbtn.io/status using requests library.

---
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+""" Gets the content of https://intranet.hbtn.io/status using requests. """
+from requests import get
+response = get('https://intranet.hbtn.io/status')
+print("Body response:")
+print("\t- type: {}".format(type(response)))
+print("\t- content: {}".format(response.text))
