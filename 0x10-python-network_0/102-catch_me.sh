37c0bba4c068e3a3a59db99659fa3575174a2a8b
DiegoCol93 <dfelo93@gmail.com>
2021-04-08 22:30:49 -0500

    | ADDED | -s flag.

--- 0x10-python-network_0/102-catch_me.sh
+++ 0x10-python-network_0/102-catch_me.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Catches the page in the challenge.
-curl 0.0.0.0:5000/catch_me -X PUT -d "user_id=98" -L -H "Origin: HolbertonSchool"
+curl -s 0.0.0.0:5000/catch_me -X PUT -d "user_id=98" -L -H "Origin: HolbertonSchool"
