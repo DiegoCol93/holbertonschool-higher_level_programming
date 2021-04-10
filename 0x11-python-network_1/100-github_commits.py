81252189830db22818591b4ed8e51a99c142707f
DiegoCol93 <dfelo93@gmail.com>
2021-04-10 00:27:51 -0500

    | FIXED | Wrong order for input arguments.

--- 0x11-python-network_1/100-github_commits.py
+++ 0x11-python-network_1/100-github_commits.py
@@ -8,8 +8,8 @@ from sys import argv as av
 from requests import get
 if __name__ == "__main__":
 
-    # Takes in "user name" as av[1] and "repository name" as av[2]
-    url = "https://api.github.com/repos/{}/{}/commits".format(av[1], av[2])
+    # Takes in "user name" as av[2] and "repository name" as av[1]
+    url = "https://api.github.com/repos/{}/{}/commits".format(av[2], av[1])
 
     r = get(url)
     commits = r.json()
