c775ca58006f0e409b9baf3bf9b3bb9fb67e0791
DiegoCol93 <dfelo93@gmail.com>
2021-04-09 22:52:23 -0500

    | ADDED | Sends a POST to query the /search_user api.

---
+++ 0x11-python-network_1/8-json_api.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+''' Sends a POST to query the /search_user api. '''
+from sys import argv as av
+from requests import post
+from json import loads
+if __name__ == '__main__':
+
+    if len(av) > 1 and bool(av[1]):
+        value = av[1]
+    else:
+        value = ''
+
+    payload = {'q': value}
+    response = post('http://0.0.0.0:5000/search_user', data=payload)
+
+    try:
+        json = loads(response.text)
+        if bool(json):
+            print("[{}] {}".format(json["id"], json["name"]))
+        else:
+            print("No result")
+    except ValueError:
+        print("Not a valid JSON")
