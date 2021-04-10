0c527d8927c1a2b44255bb144f56de5d5288d743
DiegoCol93 <dfelo93@gmail.com>
2021-04-09 23:45:18 -0500

    | ADDED | Prints my Github userid using the Github's API.

---
+++ 0x11-python-network_1/10-my_github.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+''' Prints my Github userid using the Github's API '''
+from requests import get
+from requests.auth import HTTPBasicAuth
+from sys import argv as av
+if __name__ == "__main__":
+
+    r = get('https://api.github.com/user', auth=HTTPBasicAuth(av[1], av[2]))
+    try:
+        info = r.json()
+        print(info['id'])
+    except:
+        print('None')
