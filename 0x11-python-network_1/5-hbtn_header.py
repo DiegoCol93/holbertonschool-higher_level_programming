0f6daf60bc97385b76eb040b2a5ff4c8a5916709
DiegoCol93 <dfelo93@gmail.com>
2021-04-09 19:49:41 -0500

    | ADDED | if name main.

--- 0x11-python-network_1/5-hbtn_header.py
+++ 0x11-python-network_1/5-hbtn_header.py
@@ -2,4 +2,5 @@
 """ Gets the response of a specific header https://intranet.hbtn.io/status """
 from requests import get
 from sys import argv as av
-print(get(av[1]).headers["X-Request-Id"])
+if __name__ == "__main__":
+    print(get(av[1]).headers["X-Request-Id"])
