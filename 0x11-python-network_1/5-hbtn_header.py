4448e8ddd320596bc883d5109373d1d6211fdf42
DiegoCol93 <dfelo93@gmail.com>
2021-04-09 19:57:31 -0500

    |REMOVED| if name main.

--- 0x11-python-network_1/5-hbtn_header.py
+++ 0x11-python-network_1/5-hbtn_header.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
 """ Gets the response of a specific header at the given url. """
 from requests import get
-from sys import argv as av
-if __name__ == "__main__":
-    print(get(av[1]).headers["X-Request-Id"])
+from sys import argv
+print(get(argv[1]).headers["X-Request-Id"])
