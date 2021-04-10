18aa490083500fd7b15a704208c76ff5ceac5fca
DiegoCol93 <dfelo93@gmail.com>
2021-04-09 19:42:48 -0500

    | ADDED | Gets the response of a specific header @ https://intranet.hbtn.io/status

--- 0x11-python-network_1/5-hbtn_header.py
+++ 0x11-python-network_1/5-hbtn_header.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
-""" Gets the content of https://intranet.hbtn.io/status using requests. """
+""" Gets the response of a specific header https://intranet.hbtn.io/status """
 from requests import get
 print(get('https://intranet.hbtn.io/status').headers["X-Request-Id"])
