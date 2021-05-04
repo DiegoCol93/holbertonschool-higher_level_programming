a4daabaafbd1ba215d7de688b487ec62abf28722
DiegoCol93 <dfelo93@gmail.com>
2021-05-04 14:32:39 -0500

    |- FIXED -| Wrong use of the url, simplified code.

--- 0x14-javascript-web_scraping/3-starwars_title.js
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -1,18 +1,10 @@
 #!/usr/bin/node
 const request = require('request');
 const GET = request.get;
-const url = 'https://swapi-api.hbtn.io/api/films/';
-const number = Number(process.argv[2]);
+const number = process.argv[2];
+const url = 'https://swapi-api.hbtn.io/api/films/' + number;
 
 GET(url, (err, response, body) => {
   if (err) return (console.log(err));
-
-  const data = JSON.parse(body);
-  const movies = data.results;
-
-  for (let i = 0; i < movies.length; i++) {
-    if (movies[i].episode_id === number) {
-      console.log(movies[i].title);
-    }
-  }
+  console.log(JSON.parse(body).title);
 });
